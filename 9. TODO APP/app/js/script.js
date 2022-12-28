cf9960f03bf6d646823212f1edc567de32a60e8b
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2022-12-28 10:54:39 +0100

    -RESPONSIVE LAYOUT ALMOST DONE -THEME SWITCHER DONE -DESKTOP LAYOUT DONE -CREATION , SAVING TO LOCAL STORAGE , DISPLAYING AND DELETING OF TODOS NOT YET IMPLEMENTED BUT WILL COME BACK TO IT

--- 9. TODO APP/app/js/script.js
+++ 9. TODO APP/app/js/script.js
@@ -1,18 +1,64 @@
-window.addEventListener("load", () => {
-  todos = JSON.parse(localStorage.getItem("todos")) || [];
-  const newTodoForm = document.querySelector("#new-todo-form");
+const form = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoListElement = document.getElementsByClassName("todo-item");
 
-  newTodoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+// VARIABLES
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-    const todo = {
-      content: e.target.elements.content.value,
-      createdAt: new Date().getTime(),
-    };
-    todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos));
+// LOAD TODOS FROM LOCAL STORAGE
 
-    // Reset the form
-    e.target.reset();
-  });
+// TODO: CREATE A NEW TODO
+
+// FORM SUBMIT
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  saveTodoToLocalStorage(event);
+  displayTodosFromLocalStorage();
 });
+
+// SAVE TODO
+function saveTodoToLocalStorage(event) {
+  const todoValue = todoInput.value;
+  todos.push({
+    value: todoValue,
+    checked: false,
+  });
+  event.target.reset();
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+//  DISPLAY TODOS FROM LOCAL STORAGE
+
+// RENDER TODOS
+function displayTodosFromLocalStorage() {
+  let todos = JSON.parse(localStorage.getItem("todos"));
+
+  todos.forEach((todo, index) => {
+    todoItem.innerHTML += `
+
+
+    
+    `;
+  });
+
+  // CLEAR ELEMENT BEFORE A RE-RENDER
+  // todosListEl.innerHTML = "";
+
+  // RENDER TODOS
+  //   todos.forEach((todo, index) => {
+  //     todoListElement.innerHTML += `
+
+  // `;
+  //   });
+}
+
+// CLICK EVENT LISTENER FOR ALL THE TODOS
+todoListElement.addEventListener("click", (event) => {});
+
+// DELETE TODOS FROM LOCAL STORAGE
+function deleteTodo(todoId) {
+  todos = todos.filter((todo, index) => index !== todoId);
+  // RE_RENDER
+  displayTodos();
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
