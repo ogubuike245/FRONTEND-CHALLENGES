13b9568a0daee4d0af68485e147d5d9a7c72f924
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-06-25 23:17:57 +0100

    desktop view done for all pages , nav menu and mobile view next before refactoring

---
+++ FEM/space-tourism-website-main/starter-code/assets/js/crew.js
@@ -0,0 +1,44 @@
+const dots = document.querySelectorAll(".dot");
+const crewArticles = document.querySelectorAll(
+  ".crew-section .content article"
+);
+
+// Initially show the article with id "douglas"
+crewArticles.forEach((article) => {
+  article.style.display = "none";
+
+  if (article.id === "douglas") {
+    article.style.display = "flex";
+
+    const correspondingDot = document.querySelector(".dots #" + article.id);
+    if (correspondingDot) {
+      correspondingDot.classList.add("active-dot");
+    }
+  }
+});
+
+// Show the article corresponding to the clicked dot
+dots.forEach((dot) => {
+  dot.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const clickedDot = event.currentTarget;
+    const targetArticleId = clickedDot.id;
+
+    dots.forEach((dot) => {
+      if (dot.id === targetArticleId) {
+        dot.classList.add("active-dot");
+      } else {
+        dot.classList.remove("active-dot");
+      }
+    });
+
+    crewArticles.forEach((article) => {
+      if (article.id === targetArticleId) {
+        article.style.display = "flex";
+      } else {
+        article.style.display = "none";
+      }
+    });
+  });
+});
