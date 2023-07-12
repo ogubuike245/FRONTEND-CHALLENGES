1491b0edf1104b7f954734d3adc19cdc4c8f6495
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-07-12 20:45:25 +0100

    url shortening  form design  design to do , but functionality has been set up

--- FEM/url-shortening-api-master/code/src/pages/components/FormInput.jsx
+++ FEM/url-shortening-api-master/code/src/pages/components/FormInput.jsx
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormInput = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [shortenedLinks, setShortenedLinks] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (inputValue.trim() === "") {
+      // Show error message if the input field is empty
+      alert("Please enter a valid URL");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${inputValue}`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        const shortenedLink = {
+          originalLink: inputValue,
+          shortenedLink: data.result.short_link,
+        };
+
+        setShortenedLinks((prevLinks) => [...prevLinks, shortenedLink]);
+        setInputValue("");
+      } else {
+        alert(data.error);
+      }
+    } catch (error) {
+      alert("An error occurred. Please try again later.");
+    }
+  };
+
+  const copyToClipboard = (link) => {
+    navigator.clipboard.writeText(link);
+    alert("Link copied to clipboard!");
+  };
+
   return (
     <div className="form-input">
-      <input type="text" placeholder="Shorten a link here..." />
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Shorten a link here..."
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Shorten It!</button>
+      </form>
 
-      <button>Shorten It!</button>
+      {shortenedLinks.map((link) => (
+        <div key={link.shortenedLink} className="shortened-link">
+          <p>{link.originalLink}</p>
+          <div>
+            <a
+              href={link.shortenedLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {link.shortenedLink}
+            </a>
+            <button onClick={() => copyToClipboard(link.shortenedLink)}>
+              Copy
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- FEM/url-shortening-api-master/code/src/pages/components/Navigation.jsx
+++ FEM/url-shortening-api-master/code/src/pages/components/Navigation.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const Navigation = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="container">
       <article className="nav-links">
@@ -8,21 +15,33 @@ const Navigation = () => {
           <img src="/images/logo.svg" alt="" />
         </div>
 
-        <ul>
+        <motion.ul
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          className={` ${isMobileMenuOpen ? "active" : ""}`}
+        >
           <li>Features</li>
           <li>Pricing</li>
           <li>Resources</li>
-        </ul>
+        </motion.ul>
       </article>
 
-      <article className="actions">
+      <motion.article
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.3 }}
+        className={`actions ${isMobileMenuOpen ? "active" : ""}`}
+      >
         <button>Login</button>
         <button>Sign Up</button>
-      </article>
+      </motion.article>
 
       <article className="menu-icons">
-        <i class="fa fa-bars "></i>
-        <i class="fa fa-times"></i>
+        <i
+          className={`fa ${isMobileMenuOpen ? "fa-times" : "fa-bars"}`}
+          onClick={toggleMobileMenu}
+        ></i>
       </article>
     </nav>
   );
